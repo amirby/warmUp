b1764656bec073ea7ba8621b2a3a763829d39111
amirby <amirbenyoussef3@gmail.com>
2020-02-07 09:53:16 +0100

    Amir Ben Youssef warmUp6

--- warmUp6/warmUp6.js
+++ warmUp6/warmUp6.js
@@ -2,19 +2,32 @@
 // objKey({name:'ahmed',age:20}) ==> ['name','age']
 
 function objKey(obj) {
-  // your code here...
+ var arr = [];
+ for (var keys in obj ){
+   arr.push(keys);
+ }
+ return arr;
 }
 
 // 2 - Write a function named objLength to get the length of a JavaScript object
 // objLength({name:'ahmed',age:20}) ==> 2
 
 function objLength(obj) {
-  // your code here...
+  var count = 0;
+ for (var keys in obj ){
+   count++;
+ }
+ return count;
 }
 
 // 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
 // objSort([{id:2,value:50},{id:0,value:70},{id:1,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
 
-function objSort(obj) {
-  // your code here...
+function objSort(array) {
+  for (var i = 0; i < array.length ; i++ ){
+    array.sort(function (a, b) {
+      return (a.id - b.id);
+    })
+  }  
+  return array
 }
