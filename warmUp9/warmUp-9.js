559975332161518161dbc6dc197f5107aac3bb07
amirby <amirbenyoussef3@gmail.com>
2020-02-12 09:14:24 +0100

    Amir ben Youssef warmUp9

--- warmUp9/warmUp-9.js
+++ warmUp9/warmUp-9.js
@@ -3,6 +3,16 @@
 // -name of males in your class
 // -name of your class instructors 
 
+var FemalesInClass = ['Nouha', 'Siwar']
+var MalesInClass = ['walaa', 'Aymen', 'Amine', 'Mootaz', 'Kheireddine', 'Houssem', 'Taha']
+var Instructor = ['Cassels', 'Walid', 'Seif', 'Matt']
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
-
+function middle (array){
+    return array[Math.floor((array.length)/2)]
+}
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+function multieven (array){
+    for (var i = 0; i<array.length; i= i +2){
+        return array[i] = array[i]*2;
+    }
+}
